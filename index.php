b6fd118f945854da8fd70f0642166637e1781ac0
Rachad ADEKAMBI <adekambirachad@gmail.com>
2022-02-03 21:20:36 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+     <link rel="stylesheet" href="public/css/style.css">
+    <title>Fares Immobilier</title>
+</head>
+<body>
+    <div class="header">
+
+    </div>
+
+    <div class="heading">
+        <h1 class="heading___title">
+            Fares Immobilier
+        </h1>
+
+        <div class="heading__search">
+            <ul>
+                <li>
+                    <div class="icon">
+
+                    </div>
+
+                    Appartements
+                </li>
+
+                <li>
+                    <div class="icon">
+
+                    </div>
+
+                    Bureaux
+                </li>
+
+                <li>
+                    <div class="icon">
+
+                    </div>
+
+                    Bureaux
+                </li>
+                <li>
+                    <div class="icon">
+
+                    </div>
+
+                    Restaurants
+                </li>
+
+                <li>
+                    <div class="icon">
+
+                    </div>
+
+                    Studios
+                </li>
+
+                <li>
+                    <div class="icon">
+
+                    </div>
+
+                    Villas
+                </li>
+            </ul>
+
+            <div class="heading__search__bar">
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
